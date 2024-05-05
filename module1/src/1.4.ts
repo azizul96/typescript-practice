008d35d1db259f857c2faa7eb8014ddbb781c207
Azizul Islam <alifalamin42@gmail.com>
2024-05-05 13:18:33 +0600

    3.1 module done

--- module1/src/1.4.ts
+++
@@ -1,6 +0,0 @@
-// Basic data type
-
-// String
-let firstName : string = "Alif"
-
-console.log(firstName)
--- module1/src/1.7.ts
+++
@@ -1,4 +0,0 @@
-const bros1 : string[] = ["shimul", "hafiz", "rafsan"]
-const bros2 : string[] = ["reza", "alif", "sakil"]
-
-bros1.push(...bros2)

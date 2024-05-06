c9aba66f4a4f54ab532344300267fb608a89c726
Azizul Islam <alifalamin42@gmail.com>
2024-05-06 10:43:38 +0600

    3.2 module done

--- module1/dist/1.10.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-{
-    var fullstackDeveloper = {
-        skills: ["HTML", "CSS", "EXPRESS"],
-        designation1: "Frontend Developer",
-        designation2: "Backend Developer",
-    };
-    //
-}
---
+++ module3/3.2.ts
@@ -0,0 +1,53 @@
+{
+  // oop - inheritence
+  class Parent {
+    name: string;
+    age: number;
+    address: string;
+
+    constructor(name: string, age: number, address: string) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+    }
+    getSleep(numOfHours: number) {
+      console.log(`${this.name} will sleep for ${numOfHours}`);
+    }
+  }
+
+  const person1 = new Parent("Mr. Person", 20, "Uganda");
+  person1
+  
+
+// extend from parent
+
+  class Student extends Parent {
+    constructor(name: string, age: number, address: string) {
+      super(name, age, address)
+    }
+   
+  }
+
+  const student1 = new Student("Mr. student", 20, "Uganda");
+  student1
+
+
+// extend from parent
+
+  class Teacher extends Parent{
+    designation: string
+
+    constructor(name: string, age: number, address: string,designation: string) {
+      super(name, age , address)
+      this.designation = designation
+    }
+   
+   takeClass(numOfClass: number){
+    console.log(`${this.name} will take ${numOfClass}`);
+   }
+  }
+
+  const teacher = new Teacher("Mr. teacher", 40, "Uganda","professor");
+  teacher
+  //
+}
